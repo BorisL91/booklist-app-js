5e4286608d8c5550e799e3ff34cb649c9a93c1fa
Boris <boris.lukanovic17@gmail.com>
2020-03-09 12:29:41 +0100

    finish up

--- app.js
+++ app.js
@@ -9,26 +9,12 @@ class Book {
 //UI Class: Handle UI Tasks
 class UI {
   static displayBooks() {
-    const storedBooks = [
-      {
-        title: "Book One",
-        author: "John Doe",
-        isbn: "12335634"
-      },
-      {
-        title: "Book Two",
-        author: "John Doe",
-        isbn: "123356674"
-      }
-    ]
-
-    const books = storedBooks
+    const books = Store.getBooks()
     books.forEach(book => UI.addBookToList(book))
   }
 
   static addBookToList(book) {
     const list = document.querySelector("#book-list")
-
     const row = document.createElement("tr")
     row.innerHTML = `
 			<td>${book.title}</td>
@@ -43,15 +29,19 @@ class UI {
     if (el.classList.contains("delete")) {
       el.parentElement.parentElement.remove()
     }
+    UI.showAlert("Book deleted!", "info")
   }
 
   static showAlert(message, className) {
     const div = document.createElement("div")
+    //adds bootstrap class e.g alert-danger
     div.className = `alert alert-${className}`
     div.appendChild(document.createTextNode(message))
     const container = document.querySelector(".container")
     const form = document.querySelector("#book-form")
     container.insertBefore(div, form)
+    //Vanish in 3s
+    setTimeout(() => document.querySelector(".alert").remove(), 3000)
   }
 
   static clearFields() {
@@ -60,6 +50,31 @@ class UI {
 }
 
 // Store Class: Handles Storage
+class Store {
+  static getBooks() {
+    let books
+    if (localStorage.getItem("books") === null) {
+      books = []
+    } else {
+      books = JSON.parse(localStorage.getItem("books"))
+    }
+    return books
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks()
+    books.push(book)
+    localStorage.setItem("books", JSON.stringify(books))
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks()
+    books.forEach((book, index) => {
+      books.splice(index, 1)
+    })
+    localStorage.setItem("books", JSON.stringify(books))
+  }
+}
 
 //Event: Display Books
 document.addEventListener("DOMContentLoaded", UI.displayBooks)
@@ -83,6 +98,11 @@ const form = document
 
       //Add book to UI
       UI.addBookToList(book)
+      //Add book to Store
+      Store.addBook(book)
+
+      //Add success message
+      UI.showAlert("Book added!", "success")
       //Clear fields
       UI.clearFields()
     }
@@ -91,4 +111,7 @@ const form = document
 //Event: Remove Book
 document.querySelector("#book-list").addEventListener("click", e => {
   UI.deleteBook(e.target)
+
+  //remove book from storage
+  Store.removeBook(e.target.parentElement.previousSibling.textContent)
 })
